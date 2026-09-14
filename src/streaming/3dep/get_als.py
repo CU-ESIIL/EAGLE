eb6e11f43e7935251f26f5b058c5ea2057cc5ff3
sammlapp <sammlapp@gmail.com>
2026-09-14 11:38:49 -0600

    pair datasets w ALS: butterflies, bbs, ofo

--- src/streaming/3dep/get_als.py
+++ src/streaming/3dep/get_als.py
@@ -3,8 +3,9 @@ from shapely.geometry import Point
 from pathlib import Path
 # metadata table of available USGS 3DEP 
 
-resources_aws = gpd.read_file(Path(__file__).parent / "usgs_3dep_resources_AWS.geojson")
-resources_usgs = gpd.read_file(Path(__file__).parent / "usgs_3dep_resources_ALL.geojson")
+data_root = Path(__file__).parent.parent.parent.parent / "datasets/USGS_3dep"
+resources_aws = gpd.read_file(data_root / "usgs_3dep_resources_AWS.geojson")
+resources_usgs = gpd.read_file(data_root / "usgs_3dep_resources_ALL.geojson")
 
 def lookup_3dep_by_latlon(lat, lon, earliest_year=None, latest_year=None, source="AWS"):
     """
@@ -32,3 +33,31 @@ def lookup_3dep_by_latlon(lat, lon, earliest_year=None, latest_year=None, source
     if latest_year is not None:
         matches = matches[matches['collection_year'] <= latest_year]
     return matches
+
+import functools
+
+@functools.lru_cache()
+def get_nearest_year_product(lat, lon, year, source="AWS"):
+    """
+    Get the nearest 3DEP product for a given latitude, longitude, and year.
+    
+    Parameters:
+    lat (float): Latitude of the point (CRS: EPSG:4326).
+    lon (float): Longitude of the point (CRS: EPSG:4326).
+    year (int): Year to find the nearest product.
+
+    Returns:
+    pd.Series: A Series containing the nearest 3DEP LAS data product information,
+    or None if no products are found.
+    """
+    matches = lookup_3dep_by_latlon(lat, lon, source=source)
+    if matches.empty:
+        return None
+    matches['year_diff'] = matches['collection_year'].apply(lambda x: abs(x - year))
+    nearest = matches.loc[matches['year_diff'].idxmin()]
+
+    return {
+        f"product_name_{source}": nearest['name'],
+        f"collection_year_{source}": int(nearest['collection_year']),
+        f"year_diff_{source}": int(nearest['year_diff']),
+    }
